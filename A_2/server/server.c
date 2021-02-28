c134a8360739314782c2fa96f24e305c1b903924
Leonardo Schluter <leonardoschluter@users.noreply.github.com>
2021-02-28 20:40:04 -0300

    Extract socket related code to specific file
    Used the server_socket in a server app to make hello world with
    client

--- A_2/server/server.c
+++ A_2/server/server.c
@@ -1,40 +1,20 @@
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <stdio.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
-int main() {
-    int server_sockfd, client_sockfd;
-    socklen_t server_len, client_len;
-    struct sockaddr_in server_address;
-    struct sockaddr_in client_address;
-    char buffer[1024]; 
-    char* hello = "Hello world\0";
-    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = 8080;
-    server_len = sizeof(server_address);
-    if(bind(server_sockfd, (struct sockaddr *)&server_address, server_len)<0){
-        perror("Binding failed");
-    }
-    listen(server_sockfd, 5);   
+#include <sys/types.h>
+#include "server_socket.h"
+
+int main() { 
+    int server_sockfd = openServerSocket();    
+    struct sockaddr_in* client_address = malloc(sizeof (struct soockaddr_in*));
     while(1) {
-        printf("server waiting for calculation \n");
-        client_len = sizeof(client_address);
-        printf("server: empty buffer = %s\n", buffer);
-        client_sockfd = accept(server_sockfd,(struct sockaddr *)&client_address, &client_len);
         
-        printf("server: empty buffer = %s\n", buffer);
-        bzero(buffer, 1024);
-        printf("server: empty buffer = %s\n", buffer);
-        int res = read(client_sockfd, buffer, sizeof(buffer));
-        printf("char from server = %s\n", buffer);
-        write(client_sockfd, hello, sizeof(hello));
-        close(client_sockfd);       
+        int client_sockfd = acceptClientConnection(client_address, server_sockfd);
+        
+        //TODO: interpret client message and construct a response;
+        printf("Client message: %s \n", readClientMessage(client_sockfd));
+
+        char* hello = "Hello world\0";
+        sendResponseClient(client_sockfd, hello);
+        closeClientConnection(client_sockfd);       
     }
 }
---
+++ A_2/server/server_socket.h
@@ -0,0 +1,18 @@
+#ifndef SERVER_SOCKET_H_   /* Include guard */
+#define SERVER_SOCKET_H_
+
+struct sockaddr_in;
+
+void closeClientConnection(int client_sockfd);
+
+int openServerSocket();
+
+int acceptClientConnection(struct sockaddr_in *client_address, int server_sockfd );
+
+char* readClientMessage(int client_sockfd);
+
+void sendResponseClient(int client_sockfd, char* msg);
+
+
+
+#endif // SERVER_SOCKET_H_
