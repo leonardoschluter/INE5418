5a5956562b8cd0ab223f0bbeed43649f15f3f47a
Leonardo Schluter <leonardoschluter@users.noreply.github.com>
2021-03-08 19:21:43 -0300

    Inital T_1 commit
    Adding the server and client socket code from A_2

---
+++ T_1/client/util/client.c
@@ -0,0 +1,44 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int connectToServer(){
+    int sockfd;
+    socklen_t len;
+    struct sockaddr_in address;
+    int result;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    address.sin_port = 8080;
+    len = sizeof(address);
+    result = connect(sockfd, (struct sockaddr *)&address, len);
+    if(result == -1) {
+        perror("oops: client1");
+        exit(1);
+    }
+    return sockfd;
+}
+
+void closeConnection(int sockfd){
+    close(sockfd);
+}
+
+void sendMessage(char* msg, int sockfd){   
+    printf("next command -> %s\n", msg);
+    write(sockfd, msg, sizeof(msg));
+}
+
+char* readMessage(int sockfd){
+    char *buffer = malloc(2048);
+    bzero(buffer, 2048);
+    read(sockfd, buffer, sizeof(buffer));
+    return buffer;
+
+}
---
+++ T_1/server/util/server_socket.c
@@ -0,0 +1,51 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void closeClientConnection(int client_sockfd){
+    close(client_sockfd);
+}
+
+int openServerSocket(){
+
+    int server_sockfd;
+    socklen_t server_len;
+    struct sockaddr_in server_address;
+
+    server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = 8080;
+    server_len = sizeof(server_address);
+    if(bind(server_sockfd, (struct sockaddr *)&server_address, server_len)<0){
+        perror("Binding failed");
+    }
+    listen(server_sockfd, 5);  
+    return server_sockfd;
+
+}
+
+int acceptClientConnection(struct sockaddr_in *client_address, int server_sockfd ){
+    printf("server waiting for client connection \n");
+    socklen_t client_len = sizeof(*client_address);
+    return accept(server_sockfd,(struct sockaddr *)client_address, &client_len);
+     
+}
+
+char* readClientMessage(int client_sockfd){
+    char *buffer = malloc(1024);
+    bzero(buffer, 1024);
+    read(client_sockfd, buffer, sizeof(buffer));
+    printf("buffer da msg -> %s \n", buffer);
+    return buffer;
+}
+
+void sendResponseClient(int client_sockfd, char* msg){
+    write(client_sockfd, msg, sizeof(msg));
+}
