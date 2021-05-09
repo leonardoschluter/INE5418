6067a93a8dd7b9606665e9940937b8e33c35d0c5
Leonardo Schluter <leonardoschluter@users.noreply.github.com>
2021-05-08 22:00:29 -0300

    Add Player with an address and points Add set/gets & toSerializible to communicate via string Added addPoints which accumulate points

---
+++ T_2/projeto/src/main/java/rpg_chat/Player.java
@@ -0,0 +1,48 @@
+package rpg_chat;
+
+import org.jgroups.Address;
+
+import java.io.Serializable;
+
+public class Player implements Serializable {
+    Address address;
+    private int points = 0;
+    private boolean hasBeenLeader;
+
+    public Player(Address address){
+        this.address = address;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public void addPoints(int points) {
+        this.points = this.points + points;
+    }
+
+    public void setIsLeader() {
+        this.hasBeenLeader = true;
+    }
+
+    public boolean hasBeenLeader() {
+        return hasBeenLeader;
+    }
+
+    @Override
+    public String toString(){
+        return "Address:"+address+" - Points:"+points+" - "+" hasBeenLeader"+hasBeenLeader;
+    }
+
+    public String toSerializable() {
+        return address+"|"+points+"|"+hasBeenLeader;
+    }
+
+    public void resetLeader() {
+        this.hasBeenLeader = false;
+    }
+}
