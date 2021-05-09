e63ab900a44140d6728ff02180af72cc3864ab30
Leonardo Schluter <leonardoschluter@users.noreply.github.com>
2021-05-08 22:05:44 -0300

    Add Answer class
    This is intended to the leader receive an answer from players
    before consolidating a score.

--- T_2/projeto/src/main/java/rpg_chat/Answer.java
+++ T_2/projeto/src/main/java/rpg_chat/Answer.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class Answer {
     String address;
     String text;
+    private int points = 0;
     public Answer(String address, String text){
         this.text = text;
         this.address = address;
@@ -23,5 +24,18 @@ public class Answer {
         Answer person = (Answer) o;
         // field comparison
         return Objects.equals(text, person.text);
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString(){
+        return "Player: "+address+" Text: "+text+" - points: "+points;
     }
 }
